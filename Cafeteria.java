4daa144fb1cdf63b641fd05bace90f7f4547cce3
VLADIMSHEV <86731497+VLADIMSHEV@users.noreply.github.com>
2022-03-12 21:35:54 +0300

    rename class

--- Cafeteria.java
+++ Cafeteria.java
@@ -1,43 +1,43 @@
 
-public class PhraseOMatic {
-//	Программа помогает человеку выбрать желаемый ужин, напиток и десерт.
-	public static void main(String[] args) {
+public class Cafeteria {
+    //	Программа помогает человеку выбрать желаемый ужин, напиток и десерт.
+    public static void main(String[] args) {
 
 
 //	Создаем три массива с товаром
-		String[] wordListOne = {"спагетти", "суши", "салат Цезарь", "гречку"};
+        String[] wordListOne = {"спагетти", "суши", "салат Цезарь", "гречку"};
 
-		String[] wordListTwo = {"Pepsi", "апельсиновый сок", "зеленый чай", "компот"};
+        String[] wordListTwo = {"Pepsi", "апельсиновый сок", "зеленый чай", "компот"};
 
-		String[] wordListThree = {"торт наполеон","чизкейк","вишневый пирог","печенье"};
+        String[] wordListThree = {"торт наполеон","чизкейк","вишневый пирог","печенье"};
 
 
 //	Вычисляем, сколько слов в каждом списке
 
-		int oneLength = wordListOne.length;
-		int twoLength = wordListOne.length;
-		int threeLength = wordListOne.length;
+        int oneLength = wordListOne.length;
+        int twoLength = wordListOne.length;
+        int threeLength = wordListOne.length;
 
 //	Генерируем три сулчайных числа
 
-		int rand1 = (int) (Math.random() * oneLength);
-		int rand2 = (int) (Math.random() * twoLength);
-		int rand3 = (int) (Math.random() * threeLength);
+        int rand1 = (int) (Math.random() * oneLength);
+        int rand2 = (int) (Math.random() * twoLength);
+        int rand3 = (int) (Math.random() * threeLength);
 
 //		Create  phrase
 
 //		String phrase = wordListOne[rand1] + " " + wordListTwo[rand2] + " " + wordListThree[rand3];
-		String prime = wordListOne[rand1];
-		String drink = wordListTwo[rand2];
-		String disert = wordListThree[rand3];
+        String prime = wordListOne[rand1];
+        String drink = wordListTwo[rand2];
+        String disert = wordListThree[rand3];
 
 
 
 //		Выводим фразу на экран
-		System.out.println("Хочу заказать " + prime+ ", " + drink + " и "+ disert );
+        System.out.println("Хочу заказать " + prime+ ", " + drink + " и "+ disert );
 
 
 
-	}
+    }
 
 }
