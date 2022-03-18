41f083db0597252710932f6e36bfcce8c6cb74d6
VLADIMSHEV <86731497+VLADIMSHEV@users.noreply.github.com>
2022-03-18 04:00:17 +0300

    new html

--- demo/src/main/java/com/example/demo/servingwebcontent/GreetingController.java
+++ demo/src/main/java/com/example/demo/servingwebcontent/GreetingController.java
@@ -1,17 +1,40 @@
 package com.example.demo.servingwebcontent;
 
-        import org.springframework.stereotype.Controller;
-        import org.springframework.ui.Model;
-        import org.springframework.web.bind.annotation.GetMapping;
-        import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
-
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    @GetMapping("/")
+    public String logpass(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
-        return "greeting";
+        return "login";
+    }
+
+    @GetMapping("/home")
+    public String home(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "home";
+    }
+
+    @GetMapping("/morning")
+    public String morning(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "morning";
+    }
+
+    @GetMapping("/lunch")
+    public String lunch(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "lunch";
+    }
+
+    @GetMapping("/evening")
+    public String evening(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "evening";
     }
 
 }
